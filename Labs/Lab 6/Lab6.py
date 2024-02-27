bdf0333b8bbb86920b1628da3a5a110a54e613c1
Panupong Chaphuphuang <158023141+pach2648@users.noreply.github.com>
2024-02-27 13:44:48 -0700

    Updated Lab6.py

--- Labs/Lab 6/Lab6.py
+++ Labs/Lab 6/Lab6.py
@@ -1,25 +1,123 @@
-# Pre lab 6
 import numpy as np
+import math
+import time
+from numpy.linalg import inv
+from numpy.linalg import norm
+import matplotlib.pyplot as plt;
 
-def f(x):
-    return np.cos(x)
+def driver():
+    #x0 = np.array([0.1, 0.1, -0.1])
+    x0 = np.array([1,0])
 
-x_point = np.pi / 2
+    Nmax = 100
+    tol = 1e-10
 
-h = 0.01 * 2.**(-np.arange(0, 10))
+    t = time.time()
+    for j in range(20):
+      [xstar,xlist,ier,its] =  Newton(x0,tol,Nmax);
+    elapsed = time.time()-t;
+    print(xstar);
+    print("Time : ", elapsed)
+    err = np.sum((xlist-xstar)**2,axis=1);
+    plt.plot(np.arange(its),np.log10(err[0:its]));
+    plt.xlabel("Iterations")
+    plt.ylabel("log_10(err)")
+    plt.title("Newton")
+    plt.show();
 
-# Forward difference method
-f_diff_approx = (f(x_point + h) - f(x_point)) / h
+    t = time.time()
+    for j in range(20):
+      [xstar,xlist,ier,its] =  LazyNewton(x0,tol,Nmax);
+    elapsed = time.time()-t
+    print(xstar);
+    print("Time : ", elapsed)
+    err2 = np.sum((xlist-xstar)**2,axis=1);
+    plt.plot(np.arange(its),np.log10(err2[0:its]));
+    plt.xlabel("Iterations")
+    plt.ylabel("log_10(err)")
+    plt.title("Slacker Newton")
+    plt.show();
 
-# Centered Difference method
-c_diff_approx = (f(x_point + h) - f(x_point - h)) / (2*h)
 
-# Print the results
-print("\n|__h__|__Forward Difference__|__Centered Difference__|")
-print("-" * 55)
-for i in range(len(h)):
-    print(f"|__{h[i]:.10f}__|__{f_diff_approx[i]:.10f}__|__{c_diff_approx[i]:.10f}__|")
+def evalF(x):
+
+    '''F = np.zeros(3)
+    F[0] = 3*x[0]-math.cos(x[1]*x[2])-1/2
+    F[1] = x[0]-81*(x[1]+0.1)**2+math.sin(x[2])+1.06
+    F[2] = np.exp(-x[0]*x[1])+20*x[2]+(10*math.pi-3)/3'''
+
+    F = np.zeros(2)
+    F[0] = 4*x[0]**2+x[1]**2-4
+    F[1] = x[0]+x[1]-np.sin(x[0]-x[1])
+    
+    return F
+
+def evalJ(x):
 
 
-print("The order of forward difference method is the first-order")
-print("The order of centered difference method is the second-order")
+    '''J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])],
+        [2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])],
+        [-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])'''
+    J = np.array([[8*x[0], 2*x[1]],[1-np.cos(x[0]-x[1]),1+np.cos(x[0]-x[1])]])
+    return J
+
+
+def Newton(x0,tol,Nmax):
+
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+    xlist = np.zeros((Nmax+1,len(x0)));
+    xlist[0] = x0;
+
+    for its in range(Nmax):
+       J = evalJ(x0);
+       F = evalF(x0);
+
+       x1 = x0 - np.linalg.solve(J,F);
+       xlist[its+1]=x1;
+
+       if (norm(x1-x0) < tol*norm(x0)):
+           xstar = x1
+           ier =0
+           return[xstar, xlist,ier, its];
+
+       x0 = x1
+
+    xstar = x1
+    ier = 1
+    return[xstar,xlist,ier,its];
+
+def LazyNewton(x0,tol,Nmax):
+
+    ''' Lazy Newton = use only the inverse of the Jacobian for initial guess'''
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    xlist = np.zeros((Nmax+1,len(x0)));
+    xlist[0] = x0;
+
+    J = evalJ(x0);
+    for its in range(Nmax):
+            
+       F = evalF(x0)
+       x1 = x0 - np.linalg.solve(J,F);
+       xlist[its+1]=x1;
+
+       if (norm(x1-x0) < tol*norm(x0)):
+           xstar = x1
+           ier =0
+           return[xstar,xlist, ier,its];
+
+       # Update J every 4 iterations 
+       if np.mod(its, 4) == 0:
+           J = evalJ(x0)
+
+       x0 = x1
+
+    xstar = x1
+    ier = 1
+    return[xstar,xlist,ier,its];
+
+if __name__ == '__main__':
+    # run the drivers only if this is called from the command line
+    driver();
