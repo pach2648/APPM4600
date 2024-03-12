bae744c7a3c81ac28d7fca0878a04fbd87ea6984
Panupong Chaphuphuang <158023141+pach2648@users.noreply.github.com>
2024-03-11 20:47:34 -0600

    Added Pre lab 8

---
+++ Labs/Lab 8/Pre lab 8/PreLab8.py
@@ -0,0 +1,32 @@
+def evaluate_line(x0, y0, x1, y1, alpha):
+    """
+    Inputs: x0 : x-coordinate of the 1st point
+            y0 : y-coordinate of the 1st point
+            x1 : x-coordinate of the 2nd point
+            y1 : y-coordinate of the 2nd point
+            alpha : Point at which the line will be evaluated.
+        
+    Outputs: y_alpha : The value of the line at point alpha.
+    """
+    # Calculate the slope of the line
+    slope = (y1 - y0) / (x1 - x0)
+    
+    # Calculate the y-intercept of the line using point-slope form: y - y0 = m(x - x0)
+    y_intercept = y0 - slope * x0
+    
+    # Evaluate the line at point alpha using the equation of a line: y = mx + c
+    y_alpha = slope * alpha + y_intercept
+    
+    return y_alpha
+
+#####################################
+# Main
+# Example
+x0 = 1
+y0 = 2
+x1 = 3
+y1 = 4
+alpha = 2.5
+
+result = evaluate_line(x0, y0, x1, y1, alpha)
+print("The y value of the line at alpha =", alpha, "is", result)
