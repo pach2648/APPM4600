66e78aaae53e8e29459272234e8f7de799148621
Panupong Chaphuphuang <158023141+pach2648@users.noreply.github.com>
2024-04-08 21:26:06 -0600

    Updated Pre Lab 11

--- Labs/lab 11/Pre_lab_11.py
+++ Labs/lab 11/Pre_lab_11.py
@@ -4,15 +4,16 @@ def cal_trapezoidal(a, b, f, N):
     a : Lower limit of the interval.
     b : Upper limit of the interval.
     f : The function f(x).
-    N : Number of subintervals.
+    N : Number of points.
 
     Returns:
     result: Summation from trapezoidal.
     """
-    h = (b - a) / N # Step size
+    N_int = N - 1
+    h = (b - a) / N_int # Step size
     result = f(a) + f(b)
 
-    for i in range(1, N):
+    for i in range(1, N_int):
         k = a + i * h
         result += 2*f(k)
 
@@ -24,19 +25,20 @@ def cal_simpson(a, b, f, N):
     a : Lower limit of the interval.
     b : Upper limit of the interval.
     f : The function f(x).
-    N : Number of subintervals. *****N should be even.
+    N : Number of point. *****N should be even.
 
     Returns:
     Result: Summation from simpson.
     """
+    N_int = N - 1
     
-    if N % 2 != 0:
-        return print("Number of subintervals (N) must be even for Simpson's rule.")
+    if N_int % 2 != 0:
+        return print("Number of points (N) must be odd for Simpson's rule.")
 
-    h = (b - a) / N # Step size
+    h = (b - a) / N_int # Step size
     result = f(a) + f(b)
 
-    for i in range(1, N):
+    for i in range(1, N_int):
         k = a + i * h
         # Sum of even terms
         if i % 2 == 0:
@@ -53,9 +55,9 @@ def f(x):
 
 a = 0
 b = 1
-N = 100
-integral_approx = cal_trapezoidal(a, b, f, N)
-print("Approximation of the integral using composite Trapezoidal rule:", integral_approx)
+N = 101
+integral_approx_trapz = cal_trapezoidal(a, b, f, N)
+print("Approximation of the integral using composite Trapezoidal rule:", integral_approx_trapz)
 
 integral_approx_simpson = cal_simpson(a, b, f, N)
 print("Approximation of the integral using composite Simpson's rule:", integral_approx_simpson)
